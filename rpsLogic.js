4d2f0fa849eff8b8421ccd9b7bbc9228bbe4f487
Jeffato <jlb671@scarletmail.rutgers.edu>
2025-07-17 16:08:32 -0400

    Add opponent logic

--- rpsLogic.js
+++ rpsLogic.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice == 0) return "rock";
+    else if (choice == 1) return "paper";
+    else return "scissors"
+}
+
+// console.log(getComputerChoice());
